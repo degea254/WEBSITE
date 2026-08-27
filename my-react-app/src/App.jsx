c05eccb18a1a0f08427bc67782fa615fb0541838
degea <josekathata@gmail.com>
2026-08-27 11:10:19 +0300

    Refactor code structure for improved readability and maintainability

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,7 @@
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
-import Food from "./Food.jsx";
+import Card from "./Card.jsx";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Food />
-      <Footer />
-    </>
-  );
+  return <card />;
 }
 
 export default App;
---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,13 @@
+import s from "./assets/s.png";
+
+function card() {
+  return (
+    <div className="card">
+      <img src={s} alt="profile picture"></img>
+      <h2>Degea</h2>
+      <p>i code and play video games</p>
+    </div>
+  );
+}
+
+export default card;
