4821ee2cba08eec8d29a2463cf1d5be675e67f8e
degea <josekathata@gmail.com>
2026-08-28 11:35:18 +0300

    install proptypes

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,13 @@
-import Card from "./Card.jsx";
+import Student from "./Student2.jsx";
 
 function App() {
   return (
     <>
-      <Card />;
-      <Card />;
-      <Card />;
+      <Student name="Degea" age={32} isStudent={false} />
+
+      <Student name="Totti" age={22} isStudent={true} />
+
+      <Student name="kangentu" age={12} isStudent={true} />
     </>
   );
 }
