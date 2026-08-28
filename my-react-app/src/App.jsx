c5fa876f26225c840e9b5cd62e8e2b8e146ba0bd
degea <josekathata@gmail.com>
2026-08-28 12:03:31 +0300

    feat: implement UserGreeting component and update App to use it style: add welcome and login prompt styles in index.css

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,9 @@
-import Student from "./Student2.jsx";
+import UserGreeting from "./UserGreeting";
 
 function App() {
   return (
     <>
-      <Student name="Degea" age={32} isStudent={false} />
-
-      <Student name="Totti" age={22} isStudent={true} />
-
-      <Student name="kangentu" age={12} isStudent={true} />
-
-      <Student />
+      <UserGreeting isLoggedIn={true} username="devDegea" />
     </>
   );
 }
